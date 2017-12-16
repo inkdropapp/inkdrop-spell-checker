0fb471d3bbceda9e545d75c76fee15ec2e813e6a
Takuya Matsuyama <nora@odoruinu.net>
2017-12-16 22:11:32 +0900

    Use transpiled js instead of ES6

---
+++ src/spell-checker.js
@@ -0,0 +1,27 @@
+import CodeMirrorSpellChecker from 'codemirror-spell-checker-inkdrop'
+
+module.exports = {
+
+  activate () {
+    const { CodeMirror } = require('inkdrop')
+    CodeMirrorSpellChecker({
+      codeMirrorInstance: CodeMirror,
+      ignoreCodeBlocks: true
+    })
+    global.inkdrop.on('editor:init', ::this.handleEditorInit)
+  },
+
+  deactivate () {
+    const editor = global.inkdrop.getActiveEditor()
+    if (editor.codeMirror && this.originalMode) {
+      editor.codeMirror.setOption('mode', this.originalMode)
+    }
+  },
+
+  handleEditorInit (editor) {
+    const cm = editor.codeMirror
+    this.originalMode = cm.getOption('mode')
+    cm.setOption('backdrop', this.originalMode)
+    cm.setOption('mode', 'spell-checker')
+  }
+};
