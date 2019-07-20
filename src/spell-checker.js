1c05235b73d8edd0876e3681a8b5fdf0201442f9
Takuya Matsuyama <hi@craftz.dog>
2019-07-20 15:05:41 +0900

    feat(language): Support multiple languages

--- src/spell-checker.js
+++ src/spell-checker.js
@@ -1,26 +1,44 @@
-import CodeMirrorSpellChecker from 'codemirror-spell-checker-inkdrop'
+import { logger } from 'inkdrop'
+import SpellChecker from '@inkdropapp/codemirror-spell-checker'
 import CodeMirror from 'codemirror'
 
-module.exports = {
+SpellChecker(CodeMirror)
+
+class SpellCheckerPlugin {
+  config = {
+    language: {
+      type: 'string',
+      default: 'en_US',
+      enum: ['de_DE', 'en_AU', 'en_CA', 'en_GB', 'en_US', 'fr_FR']
+    }
+  }
+
   activate() {
-    CodeMirrorSpellChecker({
-      codeMirrorInstance: CodeMirror,
-      ignoreCodeBlocks: true
-    })
     global.inkdrop.onEditorLoad(this.handleEditorInit.bind(this))
-  },
+  }
 
   deactivate() {
+    this.subs.dispose()
     const editor = global.inkdrop.getActiveEditor()
     if (editor && editor.codeMirror && this.originalMode) {
       editor.codeMirror.setOption('mode', this.originalMode)
     }
-  },
+  }
 
   handleEditorInit(editor) {
     const { cm } = editor
+    const lang = inkdrop.config.get('spell-checker.language', 'en_US')
     this.originalMode = cm.getOption('mode')
+    cm.setOption('spellCheckLang', lang)
     cm.setOption('backdrop', this.originalMode)
     cm.setOption('mode', 'spell-checker')
+    cm.refresh()
+
+    this.subs = inkdrop.config.observe('spell-checker.language', lang => {
+      logger.debug('Spellchecker language changed:', lang)
+      cm.setOption('spellCheckLang', lang)
+    })
   }
 }
+
+module.exports = new SpellCheckerPlugin()
